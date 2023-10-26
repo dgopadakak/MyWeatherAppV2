bce1a2eeeb6dec201970976e46adad983cb4c0bd
dgopa <dgopadakakv3@gmail.com>
2023-10-26 13:57:24 +0300

    getWeatherByDays method added

--- app/src/main/java/com/example/myweatherappv2/MainActivity.kt
+++ app/src/main/java/com/example/myweatherappv2/MainActivity.kt
@@ -8,10 +8,13 @@ import androidx.activity.compose.setContent
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.myweatherappv2.data.WeatherModel
 import com.example.myweatherappv2.screens.MainScreen
 import com.example.myweatherappv2.ui.theme.MyWeatherAppV2Theme
+import org.json.JSONObject
 
 const val API_KEY = "fe8e617c885e4cda976150431231708"
+const val DAYS = 3
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +35,7 @@ private fun getData(city: String, context: Context) {
     val url = "https://api.weatherapi.com/v1/forecast.json?" +
             "key=$API_KEY" +
             "&q=$city" +
-            "&days=3" +
+            "&days=$DAYS" +
             "&aqi=no&alerts=no"
     val queue = Volley.newRequestQueue(context)
     val sRequest = StringRequest(
@@ -46,4 +49,40 @@ private fun getData(city: String, context: Context) {
         }
     )
     queue.add(sRequest)
+}
+
+private fun getWeatherByDays(response: String): List<WeatherModel> {
+    if (response.isEmpty()) return listOf()
+
+    val mainObject = JSONObject(response)
+    val days = mainObject.getJSONObject("forecast").getJSONArray("forecastday")
+
+    val city = mainObject.getJSONObject("location").getString("name")
+    val result = ArrayList<WeatherModel>()
+    for (i in 0 until days.length()) {
+        val item = days[i] as JSONObject
+        result.add(
+            WeatherModel(
+                city = city,
+                time = item.getString("date"),
+                currentTemp = "",
+                condition = item
+                    .getJSONObject("day")
+                    .getJSONObject("condition")
+                    .getString("text"),
+                icon = item
+                    .getJSONObject("day")
+                    .getJSONObject("condition")
+                    .getString("icon"),
+                maxTemp = item.getJSONObject("day").getDouble("maxtemp_c").toString(),
+                minTemp = item.getJSONObject("day").getDouble("mintemp_c").toString(),
+                hours = item.getJSONArray("hour").toString()
+            )
+        )
+    }
+    result[0] = result[0].copy(     // Для первого дня (это текущий день) заменяем часть информации
+        time = mainObject.getJSONObject("current").getString("last_update"),
+        currentTemp = mainObject.getJSONObject("current").getString("temp_c")
+    )
+    return result
 }
